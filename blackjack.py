2ab842b82c52e43a4c34f189f2ac6c826aa809e9
Computing GuestAcc <socguest@lg27-18.maas>
2026-04-15 15:49:31 +0100

    get main menu working and begin game logic

--- blackjack.py
+++ blackjack.py
@@ -14,6 +14,13 @@ cursor_image = pygame.image.load('blackjack/resources/player_hand.png').convert_
 cursor_image = pygame.transform.scale(cursor_image, (16 * cursor_image_scale_multiplier, 16 * cursor_image_scale_multiplier))
 pygame.mouse.set_visible(False)
 cursor_image_rect = cursor_image.get_rect()
+mouse_pos = pygame.mouse.get_pos()
+
+pygame.font.init()
+cantarell_extrabold = pygame.font.SysFont('Cantarell Extrabold', 75)
+clear_sans_bold = pygame.font.SysFont('Clear Sans Bold', 50)
+bgm_toggle_hover_text = clear_sans_bold.render('Mute music', True, (0, 0, 0))
+bgm_toggle_hover_text_outline = clear_sans_bold.render('Mute music', True, (255, 255, 0))
 
 background_music_0 = pygame.mixer.Sound('blackjack/resources/sounds/bgm0.mp3')
 background_music_0.play(-1)
@@ -24,6 +31,17 @@ bgm_off = pygame.image.load('blackjack/resources/bgm_off.png').convert_alpha()
 bgm_on_full = pygame.image.load('blackjack/resources/bgm_on_full.png').convert_alpha()
 bgm_off = pygame.transform.scale(bgm_off, (16 * bgm_icon_scale_multiplier, 16 * bgm_icon_scale_multiplier))
 bgm_on_full = pygame.transform.scale(bgm_on_full, (16 * bgm_icon_scale_multiplier, 16 * bgm_icon_scale_multiplier))
+
+player_hand = pygame.sprite.Group()
+bot_hand = pygame.sprite.Group()
+
+header_position = (800, 150)
+header_image = pygame.image.load('blackjack/resources/header.jpg').convert()
+
+play_button_position = (800, 700)
+play_button_image = pygame.image.load('blackjack/resources/play_button.jpg').convert()
+
+on_main_menu = True
 
 card_scale_multiplier = 3
 
@@ -67,12 +85,26 @@ class Card(pygame.sprite.Sprite):
         self.active_image = self.back_image
         self.being_flipped_around = False
         self.being_dragged = False
+        self.index_in_deck = 0
 
-mouse_pos = pygame.mouse.get_pos()
-player_hand = pygame.sprite.Group()
-bot_hand = pygame.sprite.Group()
+header_rect = pygame.Rect(header_position[0], header_position[1], header_image.get_width(), header_image.get_height())
+header_rect.center = header_position
 
-while running:  
+play_button_rect = pygame.Rect(play_button_position[0], play_button_position[1], play_button_image.get_width(), play_button_image.get_height())
+play_button_rect.center = play_button_position
+
+play_button_hover_text = clear_sans_bold.render('Click to play', True, (0, 0, 0)).convert_alpha()
+play_button_hover_text_outline = clear_sans_bold.render('Click to play', True, (255, 255, 0)).convert_alpha()
+
+player_hand_area = pygame.Rect(0, 675, 1600, 225)
+
+while running:
+
+    if not on_main_menu:
+        if len(player_hand.sprites()) < 2:
+            player_hand.add(Card(random.randint(1, 13), random.choice(list(suits.items()))[0], mouse_pos[0], mouse_pos[1]))
+            player_hand.add(Card(random.randint(1, 13), random.choice(list(suits.items()))[0], mouse_pos[0], mouse_pos[1] + 10))
+
 
     mouse_pos = pygame.mouse.get_pos()
     cursor_image_rect.center = mouse_pos
@@ -100,6 +132,8 @@ while running:
                 for card in player_hand:
                     if card.rect.collidepoint(mouse_pos):
                         card.being_dragged = True
+                if play_button_rect.collidepoint(mouse_pos):
+                    on_main_menu = False
             elif event.button == 3:
                 for card in player_hand:
                     if card.rect.collidepoint(mouse_pos):
@@ -118,13 +152,24 @@ while running:
             if event.button == 1:
                 for card in player_hand:
                     card.being_dragged = False
-        if event.type == pygame.KEYDOWN:
+        """if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 #spawn new random card at mouse position
-                player_hand.add(Card(random.randint(1, 13), random.choice(list(suits.items()))[0], mouse_pos[0], mouse_pos[1]))
+                new_card = Card(random.randint(1, 13), random.choice(list(suits.items()))[0], mouse_pos[0], mouse_pos[1])"""
+                
     #rendering
     screen.fill(screen_colour)
 
+    if on_main_menu:
+        screen.blit(header_image, header_rect)
+        screen.blit(play_button_image, play_button_rect)
+
+        if play_button_rect.collidepoint(mouse_pos):
+            screen.blit(play_button_hover_text_outline, (mouse_pos[0], mouse_pos[1] - 50))
+            screen.blit(play_button_hover_text, (mouse_pos[0] - 2, mouse_pos[1] - 53))
+    else:
+        pygame.draw.rect(screen, (176, 128, 58), player_hand_area)
+    
     if bgm_playing == True:
         screen.blit(bgm_on_full, bgm_icon_position)
     else:
@@ -132,7 +177,11 @@ while running:
 
     for card in player_hand:
         screen.blit(card.active_image, card.rect)
-    
+
+    if bgm_toggle.collidepoint(mouse_pos):
+        screen.blit(bgm_toggle_hover_text_outline, (mouse_pos[0] - 153, mouse_pos[1] + 38))
+        screen.blit(bgm_toggle_hover_text, (mouse_pos[0] - 150, mouse_pos[1] + 35))
+
     cursor_image_rect.center = pygame.mouse.get_pos()
     screen.blit(cursor_image, cursor_image_rect) # cursor should be the last thing blitted 
     
